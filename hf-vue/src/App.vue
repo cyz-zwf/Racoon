1899d75c10d67f8b124d7c9e35b34db8700ada4a
cyz <cyz@tedu.cn>
2019-07-31 14:00:38 +0800

    home.vue

--- hf-vue/src/App.vue
+++ hf-vue/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">浣熊 顶部导航条</router-link> 
+      <router-link to="/about">0</router-link>
     </div>
     <router-view/>
   </div>
--- hf-vue/src/router.js
+++ hf-vue/src/router.js
@@ -1,23 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+// 引入组件
+import Home from '../src/components/common/Home.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+   //为组件指定访问路径
+   {path:'/Home',component:Home}
   ]
 })
