d4f1ef52fa5f026488f7436358d3c47a66d8ab9c
mianma <mianmabb@163.com>
2019-08-02 19:58:44 +0800

    sql文件改名

---
+++ vue_app/src/components/common/Cart.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <!-- Cart.vue组件 -->
+    <header>
+      <span>购物车</span>
+    </header>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style scoped>
+
+</style>
