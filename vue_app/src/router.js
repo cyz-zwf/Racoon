45a1a4f941d0c4ccf7ea0dba8442b996aacf034c
mianma <mianmabb@163.com>
2019-08-03 17:55:57 +0800

    解决了router.js冲突

--- vue_app/src/router.js
+++ vue_app/src/router.js
@@ -2,12 +2,9 @@ import Vue from 'vue'
 import Router from 'vue-router'
 // 引入组件
 import Home from './components/Home.vue'
-<<<<<<< HEAD
 import Cart from './components/Cart/Cart.vue'
-=======
 import MyRacoon from './components/myRacoon/MyRacoon.vue'
 import Cart from './components/common/Cart.vue'
->>>>>>> 73b16b3d6af8322b11eab018a693549704fb014d
 
 Vue.use(Router)
 
