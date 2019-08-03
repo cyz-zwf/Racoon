ab17788b47a0dc75a09f42a7f4b7ce5533c2d8a7
mianma <mianmabb@163.com>
2019-08-03 10:58:43 +0800

    6

--- vue_app/src/components/common/Cart.vue
+++ vue_app/src/components/common/Cart.vue
@@ -1,8 +1,15 @@
 <template>
   <div>
     <!-- Cart.vue组件 -->
-    <header>
-      <span>购物车</span>
+    <header class="top_nav">
+      <div class="">
+        <span class="top_title">
+          <span>购物车</span>
+        </span>
+        <div class="top_btn">
+          <router-link to="/Message" title="消息中心" class="toMsg"></router-link>
+        </div>
+      </div>
     </header>
   </div>
 </template>
@@ -12,5 +19,10 @@ export default {
 }
 </script>
 <style scoped>
-
+.top_nav{
+  width: 100%;
+  height: 48px;
+  display: fiex;
+  position: fixed;
+}
 </style>
--- vue_app/src/router.js
+++ vue_app/src/router.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import Router from 'vue-router'
 // 引入组件
 import Home from './components/Home.vue'
+import Cart from './components/common/Cart.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
    //为组件指定访问路径
-   {path:'/Home',component:Home}
+   {path:'/Home',component:Home},
+   {path:'/Cart',component:Cart},
   ]
 })
