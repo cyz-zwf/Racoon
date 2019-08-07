7fb872e52f35378535aaf47a526085c454f594b7
mianma <mianmabb@163.com>
2019-08-07 09:02:48 +0800

    找回了app.js

--- vue_server/app.js
+++ vue_server/app.js
@@ -100,3 +100,66 @@ app.get("/sessinfo", (req, res) => {
         });
     }
 })
+/* 购物车功能 */
+app.get("/cart", (req, res) => {
+    var uid = req.query.uid;
+    var sql = "SELECT id,img_url,title,price,count,spec FROM racoon_cart WHERE uid=?";
+    pool.query(sql, [uid], (err, result) => {
+        if (err) throw err;
+        res.send({ code: 1, data: result });
+    })
+})
+app.get("/delItem", (req, res) => {
+    var id = req.query.id;
+    var sql = "DELETE FROM racoon_cart WHERE id =? ";
+    pool.query(sql, [id], (err, result) => {
+        if (err) throw err;
+        if (result.affectedRows > 0) {
+            res.send({ code: 1, msg: "删除成功" });
+        } else {
+            res.send({ code: -1, msg: "删除失败" })
+        }
+    })
+})
+app.get("/addCart", (req, res) => {
+    // 1.参数
+    var lid = req.query.lid;
+    var price = req.query.price;
+    var title = req.query.title;
+    var spec = req.query.spec;
+    var img_url = req.query.img_url;
+    var uid = req.query.uid;  //在没有登录功能时使用
+    //var uid = req.session.uid;  //有登录功能时使用
+    if (!uid) {
+        res.send({ code: -1, msg: "请先登录" });
+        return;
+    }
+    // console.log(lid,price,title,uid)
+    var sql = "SELECT id FROM racoon_cart WHERE lid=? AND uid=?";
+    pool.query(sql, [lid, uid], (err, result) => {
+        if (err) throw err;
+        var sql = "";
+        if (result.length == 0) {
+            sql = `INSERT INTO racoon_cart VALUES(null,${lid},${img_url},${price},'${title}',${spec},1,${uid})`
+        } else {
+            sql = `UPDATE racoon_cart SET count=count+1 WHERE lid=${lid} AND uid=${uid}`;
+        }
+        pool.query(sql, (err, result) => {
+            if (err) throw err;
+            res.send({ code: 1, msg: "添加成功" })
+        })
+    })
+})
+// 测试：
+// http://127.0.0.1:5050/addCart?lid=648&price=738&img_url="dior-perfume.webp"&title="Dior 迪奥 迪奥小姐花漾女士淡香水"&spec="100毫升"&uid=1
+
+app.get("/updateCount", (req, res) => {
+    var count = req.query.count;
+    var uid = req.query.uid;
+    var lid = req.query.lid;
+    sql = `UPDATE racoon_cart SET count=${count} WHERE lid=${lid} AND uid=${uid}`;
+    pool.query(sql, (err, result) => {
+        if (err) throw err;
+        res.send({ code: 1, msg: "更新成功" })
+    })
+})
