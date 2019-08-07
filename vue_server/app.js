1279b126f464d2a93f710028e700a9190e049f8d
baba <baba@163.com>
2019-08-07 08:35:16 +0800

    暂存

--- vue_server/app.js
+++ vue_server/app.js
@@ -9,7 +9,6 @@ const cors = require("cors");
 // session 
 const session = require("express-session");
 // 配置数据连接池
-<<<<<<< HEAD
 var pool = mysql.createPool({
     host: '127.0.0.1',
     port: 3306,
@@ -17,15 +16,6 @@ var pool = mysql.createPool({
     password: '',
     database: 'racoon',
     connectionLimit: 15,
-=======
-var pool=mysql.createPool({
-	host:'127.0.0.1',
-	port:3306,
-	user:'root',
-	password:'',
-	database:'racoon',
-	connectionLimit:15,
->>>>>>> d35337296f8f7f1892543db34285c8f949d7f604
 });
 var app = express();
 app.use(
@@ -47,7 +37,6 @@ app.listen(5050);
 
 // 主页
 app.get("/index", (req, res) => {
-<<<<<<< HEAD
     var sql = "select hid,pic,href from racoon_index_product";
     pool.query(sql, (err, result) => {
         if (err) throw err;
@@ -110,72 +99,4 @@ app.get("/sessinfo", (req, res) => {
             code: -1
         });
     }
-=======
-	var sql = "select hid,pic,href from racoon_index_product";
-	pool.query(sql, (err, result) => {
-		if (err) throw err;
-		res.send({ code: 1, msg: "查询成功", data: result });
-	});
-});
-app.get("/cart",(req,res) => {
-	var uid = req.query.uid;
-	var sql = "SELECT id,img_url,title,price,count,spec FROM racoon_cart WHERE uid=?";
-	pool.query(sql,[uid],(err,result)=>{
-		if(err) throw err;
-		res.send({code:1,data:result});
-	})
-})
-app.get("/delItem",(req,res)=>{
-	var id=req.query.id;
-	var sql = "DELETE FROM racoon_cart WHERE id =? ";
-	pool.query(sql,[id],(err,result)=>{
-		if(err) throw err;
-		if (result.affectedRows > 0) {
-			res.send({ code: 1, msg: "删除成功" });
-		} else {
-			res.send({ code: -1, msg: "删除失败" })
-		}
-	})
-})
-app.get("/addCart", (req, res) => {
-	// 1.参数
-	var lid = req.query.lid;
-	var price = req.query.price;
-	var title = req.query.title;
-	var spec = req.query.spec;
-	var img_url = req.query.img_url;
-	var uid = req.query.uid;  //在没有登录功能时使用
-	//var uid = req.session.uid;  //有登录功能时使用
-	if (!uid) {
-		res.send({ code: -1, msg: "请先登录" });
-		return;
-	}
-	// console.log(lid,price,title,uid)
-	var sql = "SELECT id FROM racoon_cart WHERE lid=? AND uid=?";
-	pool.query(sql, [lid, uid], (err, result) => {
-		if (err) throw err;
-		var sql = "";
-		if (result.length == 0) {
-			sql = `INSERT INTO racoon_cart VALUES(null,${lid},${img_url},${price},'${title}',${spec},1,${uid})`
-		} else {
-			sql = `UPDATE racoon_cart SET count=count+1 WHERE lid=${lid} AND uid=${uid}`;
-		}
-		pool.query(sql, (err, result) => {
-			if (err) throw err;
-			res.send({ code: 1, msg: "添加成功" })
-		})
-	})
-})
-// 测试：
-// http://127.0.0.1:5050/addCart?lid=648&price=738&img_url="dior-perfume.webp"&title="Dior 迪奥 迪奥小姐花漾女士淡香水"&spec="100毫升"&uid=1
-app.get("/updateCount",(req,res)=>{
-	var count=req.query.count;
-	var uid=req.query.uid;
-	var lid=req.query.lid;
-	sql = `UPDATE racoon_cart SET count=${count} WHERE lid=${lid} AND uid=${uid}`;
-	pool.query(sql,(err,result)=>{
-		if(err) throw err;
-		res.send({code:1,msg:"更新成功"})
-	})
->>>>>>> d35337296f8f7f1892543db34285c8f949d7f604
 })
